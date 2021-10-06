f5de257cee2851d149e3d226391d3b8c02bbf88d
annemarie293 <annemarie293@gmail.com>
2021-10-06 23:27:08 +0000

    add background image, heading styles

--- app.py
+++ app.py
@@ -40,7 +40,7 @@ def register():
         if password1 != password2:
             flash("Passwords do not match, please re-check")
             return redirect(url_for('register'))
-    
+            
         # Create new username/password dictionary to add to DB
         register = {
             "username": request.form.get("username").lower(),
@@ -54,6 +54,33 @@ def register():
         return redirect(url_for('base', username=session["user"]))
 
     return render_template("register.html")
+
+
+@app.route("/sign_in", methods=["GET","POST"])
+def sign_in():
+    if request.method == "POST":
+        # Check if user already exists in DB
+        user_exists = mongo.db.users.find_one(
+            {"username": request.form.get("username").lower()})
+
+        if user_exists:
+            # Check password is correct
+            if check_password_hash(user_exists["password"],
+                                   request.form.get("password")):
+                session["user"] = request.form.get("username").lower()
+                flash("Welcome {}!".format(request.form.get("username")))
+                return redirect(url_for('base', username=session["user"]))
+            else:
+                # If password is incorrect
+                flash("Username or password is incorrect, Please try again")
+                return redirect(url_for('sign_in'))
+        else:
+            # If user does not exist/username is incorrect
+            flash("Username or password is incorrect, Please try again")
+            return redirect(url_for('sign_in'))
+    
+    return render_template("sign-in.html")
+
 
 # @app.route("/all_wines")
 # def all_wines():
